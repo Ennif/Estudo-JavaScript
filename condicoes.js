10e9dd08e7248fec442065c735b9f3b7ba77fa69
Ezau Martins <ezaumateus97@gmail.com>
2020-06-09 18:29:10 -0300

    TestesCondições.

---
+++ condicoes.js
@@ -0,0 +1,18 @@
+
+
+/*var pais = 'Brasil'
+console.log(`Seu Pais de origem é ${pais}`)
+
+if (pais == 'Brasil'){
+    console.log('Brasileiro')
+} else{
+    console.log('Estrangeiro.')
+}*/
+
+
+var vel = 6.5
+console.log(`a velocidade é ${vel}km/h`)
+
+if ( vel > 60){
+    console.log(`Atingiu limite de velocidade.`)
+}
