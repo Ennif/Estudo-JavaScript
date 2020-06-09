1fb7cf7c6aa11d8b37632ecfa4f54c2f425bb6c4
Ezau Martins <ezaumateus97@gmail.com>
2020-06-09 20:14:35 -0300

    condicoesaninhadas

--- condicoes.js
+++ condicoes.js
@@ -10,9 +10,21 @@ if (pais == 'Brasil'){
 }*/
 
 
-var vel = 6.5
+/*var vel = 6.5
 console.log(`a velocidade é ${vel}km/h`)
 
 if ( vel > 60){
     console.log(`Atingiu limite de velocidade.`)
+}*/
+
+var idade = 81
+
+if (idade < 16){
+    console.log('Não Vota')
+}   else {
+        if( idade < 18 || idade>80){      
+                console.log ('opcional')
+            }else {
+                console.log ('Obrigatório')
+            }
 }
