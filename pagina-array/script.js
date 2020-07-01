74d189b565a107d89b657d2b563df65a1f97024f
Ezau Martins <ezaumateus97@gmail.com>
2020-06-30 21:49:33 -0300

    ArraysForIn

--- pagina-array/script.js
+++ pagina-array/script.js
@@ -26,10 +26,41 @@ function adicionar(){
         let item = document.createElement('option')
         item.text = `valor ${num.value} adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
         
     } else {
         window.alert('Valor invalido ou já listado')
     }
     num.value = ''
     num.focus()
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert("Adicione Valores")
+    }else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma  = 0
+        let media = 0
+            for (let pos in valores){
+                
+                
+                soma += valores[pos]
+                if (valores[pos] > maior )
+                    maior = valores[pos]
+                
+                if (valores[pos] < menor)
+                    menor = valores[pos]
+                
+            }
+        media = soma / valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} numeros cadastrados</p>` 
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma totol : ${soma}</p>`
+        res.innerHTML += `<p>Media dos valores ${media}</p>`
+    }
 }
